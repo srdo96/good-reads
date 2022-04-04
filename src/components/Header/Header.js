d17b2ea4683f0a149ed6d88c1a640a4e9dc5e970
Sakib Rahman <seu.sakib@gmail.com>
2022-04-04 22:19:07 +0600

    add hover on navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,21 @@ import "./Header.css";
 const Header = () => {
   return (
     <nav>
-      <Link to="/">HOME</Link>
-      <Link to="/reviews" className="mx-5">
+      <Link to="/" className="hover:text-yellow-400">
+        HOME
+      </Link>
+      <Link to="/reviews" className="mx-5 hover:text-yellow-300">
         REVIEWS
       </Link>
-      <Link to="/dashboard">DASHBOARD</Link>
-      <Link to="/blogs" className="mx-5">
+      <Link to="/dashboard" className="hover:text-yellow-300">
+        DASHBOARD
+      </Link>
+      <Link to="/blogs" className="mx-5 hover:text-yellow-300">
         BLOGS
       </Link>
-      <Link to="/about">ABOUT</Link>
+      <Link to="/about" className=" hover:text-yellow-300">
+        ABOUT
+      </Link>
     </nav>
   );
 };
